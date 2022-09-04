ef90b1c925c39092c7967f3ccf12e67c0448c690
LostGreiz-22 <lnnnkojp@gmail.com>
2022-09-04 22:53:55 +0800

    Added Routes to Log in and Registration pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
 import NavigationBar from './components/NavigationBar';
 import Forum from './components/pages/Forum';
+
 
 function App() {
   return (
@@ -9,6 +15,11 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/forum" element={<Forum />}/>
+        <Route exact path="/" component={ LandingPage } />
+        <Route path="/login" component={ LoginPage } />
+        <Route path="/register" component={ RegisterPage } />
+        <Route path="/forget-password" component={ ForgetPasswordPage } />
+        <Route path="/home" component={ HomePage } />
       </Routes>
    </BrowserRouter>
   );
