50ed77025eb351666fd4c4427c49252addfe681d
Lorenz <znerol.anoso@gmail.com>
2022-09-04 10:54:42 +0800

    Forum Page Component
    - Added welcome message
    - ongoing plan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import Forum from './components/pages/Forum';
 
 function App() {
   return (
    <BrowserRouter>
       <NavigationBar />
       <Routes>
-        <Route />
+        <Route path="/forum" element={<Forum />}/>
       </Routes>
    </BrowserRouter>
   );
---
+++ src/components/pages/Forum.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+
+function Forum() {
+  return (
+    <>
+      <Container className="pt-5">
+        <div class="alert alert-success rounded-0" role="alert">
+          <small className="text-muted">
+            You've entered PawerCode's Community Forum!
+          </small>
+        </div>
+      </Container>
+      <Container className="">
+        {/* <div className="row mx-1 bg-dark text-white">test</div> */}
+      </Container>
+    </>
+  );
+}
+
+export default Forum;
